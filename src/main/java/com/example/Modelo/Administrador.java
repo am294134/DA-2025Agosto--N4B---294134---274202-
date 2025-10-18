ef6863b6291f6a4ecbbc1e039dabb566d2b77a4b
GiulianaScaglia <gs274202@fi365.ort.edu.uy>
2025-10-18 12:00:42 -0300

    prueba comentario

--- src/main/java/com/example/Modelo/Administrador.java
+++ src/main/java/com/example/Modelo/Administrador.java
@@ -4,4 +4,4 @@ public class Administrador extends Usuario {
     public Administrador(String nombre, String apellido, String cedula, String contrasenia) {
         super(nombre, apellido, cedula, contrasenia);
     }
-}
+}//comentario
