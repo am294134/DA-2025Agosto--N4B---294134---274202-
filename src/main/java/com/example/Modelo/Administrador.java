adf5d7c2b66a705a1b2b322fbf281a61572fbab0
Alan Martínez <am294134@fi365.ort.edu.uy>
2025-10-18 12:07:47 -0300

    borre comentario prueba

--- src/main/java/com/example/Modelo/Administrador.java
+++ src/main/java/com/example/Modelo/Administrador.java
@@ -4,5 +4,5 @@ public class Administrador extends Usuario {
     public Administrador(String nombre, String apellido, String cedula, String contrasenia) {
         super(nombre, apellido, cedula, contrasenia);
     }
-}//comentario jbkjbbjjk
+}
 
