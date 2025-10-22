190c36102799e1db81d4b5112341c62708581808
alansitomar29 <alansitomar29@gmail.com>
2025-10-21 22:13:57 -0300

    cargo datos en main

--- src/main/java/com/example/obligatorio_dda/ObligatorioDdaApplication.java
+++ src/main/java/com/example/obligatorio_dda/ObligatorioDdaApplication.java
@@ -3,11 +3,13 @@ package com.example.obligatorio_dda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.obligatorio_dda.Modelo.Datos;
+
 @SpringBootApplication
 public class ObligatorioDdaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ObligatorioDdaApplication.class, args);
-		System.out.println("Funciona run");
+		Datos.cargar();
 	}
 }
