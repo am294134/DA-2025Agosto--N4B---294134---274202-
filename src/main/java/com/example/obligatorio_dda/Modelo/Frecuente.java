4899aa975223f5fde1db7555f55a81d925b197d0
GiulianaScaglia <gs274202@fi365.ort.edu.uy>
2025-10-31 22:37:36 -0300

    heredan de boni, falta implementar metodo overr

---
+++ src/main/java/com/example/obligatorio_dda/Modelo/Frecuente.java
@@ -0,0 +1,11 @@
+public class Frecuente extends Bonificacion {
+    public Frecuente(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void calcularDescuento() {
+        // Implementación específica para Frecuente
+    }       
+
+}
---
+++ src/main/java/com/example/obligatorio_dda/Modelo/Trabajador.java
@@ -0,0 +1,11 @@
+public class Trabajador extends Bonificacion {
+    public Trabajador(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void calcularDescuento() {
+        // Implementación específica para Trabajador
+    }       
+
+}
