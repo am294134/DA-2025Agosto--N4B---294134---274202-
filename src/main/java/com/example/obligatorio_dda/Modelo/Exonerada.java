ab5f52cd2d1fb3ed9dafa54ebb5e26033e07d27b
GiulianaScaglia <gs274202@fi365.ort.edu.uy>
2025-10-31 22:37:16 -0300

    hereda de bonif, metodo overraid falta implementar

---
+++ src/main/java/com/example/obligatorio_dda/Modelo/Exonerada.java
@@ -0,0 +1,11 @@
+public class Exonerada extends Bonificacion {
+    public Exonerada(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void calcularDescuento() {
+        // Implementación específica para Exonerada
+    }   
+
+}
