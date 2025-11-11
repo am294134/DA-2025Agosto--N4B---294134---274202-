bc979ffc40a5566a6ffbf0d01b382288b325b80a
GiulianaScaglia <gs274202@fi365.ort.edu.uy>
2025-11-10 21:48:15 -0300

    categoria borramos lista

--- src/main/java/com/example/obligatorio_dda/Modelo/Categoria.java
+++ src/main/java/com/example/obligatorio_dda/Modelo/Categoria.java
@@ -4,19 +4,15 @@ import java.util.ArrayList;
 
 public class Categoria {
     private String nombre;
-    private ArrayList<Tarifa> tarifas;
 
     public Categoria(String nombre) {
         this.nombre = nombre;
-        this.tarifas = new ArrayList<>();
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public ArrayList<Tarifa> getTarifas() {
-        return tarifas;
-    }
     
+
 }
